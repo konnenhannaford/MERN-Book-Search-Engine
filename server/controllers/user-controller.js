610afe2ef84b0a249ec2f3d8d92a3558deea8e68
konnenhannaford <konnen23@gmail.com>
2022-01-26 00:21:20 +1100

    still trying to conenct files

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,6 +3,8 @@ const { User } = require('../models');
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
 
+// do i need this?
+
 module.exports = {
   // get a single user by either their id or their username
   async getSingleUser({ user = null, params }, res) {
