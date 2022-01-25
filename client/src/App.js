f127ebd40db8a0f205bff9cfa905750487198928
konnenhannaford <konnen23@gmail.com>
2022-01-26 00:22:08 +1100

    apollo link added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// do these go in?
+// import { ApolloProvider } from '@apollo/react-hooks';
+// import ApolloClient from 'apollo-boost';
+// or these
+// import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
